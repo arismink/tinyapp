96c8640ed4507cd245896566b0bf5a2d7608644e
krismina la <krismina.la@gmail.com>
2022-03-22 20:40:29 +0000

    fixed bug in redirect link

--- express_server.js
+++ express_server.js
@@ -28,21 +28,23 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   let newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = "http://" + req.body.longURL;
+  urlDatabase[newShortURL] = req.body.longURL;
   console.log(`New short url created: ${newShortURL} for ${req.body.longURL}`);
   res.redirect(`/urls:${newShortURL}`);
 });
 
 app.get("/urls:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL.substring(1), longURL: `http://${urlDatabase[req.params.shortURL.substring(1)]}`};
+  const templateVars = { shortURL: req.params.shortURL.substring(1), longURL: urlDatabase[req.params.shortURL.substring(1)]};
+  console.log(templateVars);
 
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  console.log('shortURL', req.params.shortURL);
+  const longURL = urlDatabase[req.params.shortURL.substring(1)];
   console.log(`Redirecting to: ${longURL}`);
-  res.redirect(`http://${longURL}`); 
+  res.redirect(longURL); 
 })
 
 app.post("/urls/:shortURL/delete", (req, res) => {
