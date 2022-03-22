d3c909971f2b52591a4950deabf3e00ad3c525dc
krismina la <krismina.la@gmail.com>
2022-03-22 00:34:30 +0000

    added functionality to add new shortURL to database when POST is made

--- express_server.js
+++ express_server.js
@@ -27,8 +27,12 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);
+
+  urlDatabase[generateRandomString()] = req.body.longURL;
+
+  console.log(urlDatabase);
+  res.send("Ok");
 });
 
 app.get("/urls:shortURL", (req, res) => {
